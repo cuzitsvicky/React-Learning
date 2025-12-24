fa77bf114c604f250f4f6c680843f1081553deac
cuzitsvicky <en22cs301558@medicaps.ac.in>
2025-12-24 09:56:04 +0530

    07ContextApi

---
+++ 07ContextApi/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import UserContext from './context/UserContext'
+import Login from './components/Login'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+
+
+function App() {
+  
+  return (
+    <UserContextProvider>
+      <h1>Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
